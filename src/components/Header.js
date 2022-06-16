27221d720eaba1ecdba927f91e9addc4b3f2585c
Ali-m24 <ali24mohammadzadeh@gmail.com>
2022-06-16 12:18:43 +0430

    Create Structure for project

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Logo from "../assets/images/logo.png";
+import "../styles/Header.css";
+import { BsFillSunFill } from "react-icons/bs";
+import { FaShoppingCart } from "react-icons/fa";
+
+function Header() {
+  return (
+    <div>
+      
+    </div>
+  );
+}
+
+export default Header;
